16c4603c2cec62be8e6980723e34767da45c49f9
Nikitha Nagumalli <nikithanagumalli@yahoo.com>
2025-09-07 14:55:12 -0500

    fixed terminate problem and have threads running at same time now

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 #include <queue>
 #include <mutex>
-// #include <thread>
+#include <thread>
 #include <condition_variable>
 #include <vector>
 
@@ -21,24 +21,25 @@ using std::vector;
 class ThreadPool {
     private:
         vector<std::thread> _threads;
-        queue<tuple<int, time_t>> tasks; // TODO: why can't I make this atomic?
+        queue<tuple<int, time_t>> tasks;
         atomic<int> count = 0; // TODO: does this need to be atomic if it's only being updated in mutex locked section?
         mutex mtx;
         condition_variable cv;
+        bool _stop = false;
 
-        void process_task() {
-            cout << "here" << endl;
+        void process_task(int thread_num) {
             while (true) {
-                mtx.lock();
-                std::unique_lock<mutex> lock(mtx);
-                cv.wait(lock, [this] {return !tasks.empty();});
-                if (tasks.empty()) {
-                    return;
+                tuple<int, time_t> task;
+                {    
+                    std::unique_lock<mutex> lock(mtx);
+                    cv.wait(lock, [this] {return !tasks.empty() || _stop;});
+                    if (_stop && tasks.empty()) {
+                        return;
+                    }
+                    task = std::move(tasks.front());
+                    tasks.pop();
                 }
-                tuple<int, time_t> task = std::move(tasks.front());
-                tasks.pop();
-                mtx.unlock();
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 std::thread::id this_id = std::this_thread::get_id();
                 cout << "Task Count: " << std::get<0>(task) << " at time: " << std::get<1>(task) << " processed by " << this_id << endl;
             }
@@ -47,16 +48,20 @@ class ThreadPool {
     public:
         ThreadPool(int threads_num) {
             for (int i = 0; i < threads_num; i++) {
-                std::thread t([this] {process_task();});
-                // _threads.emplace_back(t);
+                _threads.emplace_back([this, i] {
+                    process_task(i);
+                });
             }
         }
 
         ~ThreadPool() {
-            // for (int i = 0; i < _threads.size(); i++) {
-            //     std::thread t = _threads[0];
-            //     t.join();
-            // }
+            mtx.lock();
+            _stop = true;
+            mtx.unlock();
+            cv.notify_all();
+            for (int i = 0; i < _threads.size(); i++) {
+                _threads[i].join();
+            }
         };
 
         void create_task() {
@@ -66,21 +71,17 @@ class ThreadPool {
             mtx.lock();
             tasks.push(make_tuple(++count, timestamp));
             mtx.unlock();
-            cout << "Pushed task number: " << count << endl;
+            // cout << "Pushed task number: " << count << endl;
             cv.notify_one();
             return;
         }
 };
 
 
-
-
-
-// cout << "thread id is " << t.get_id() << endl;
 int main() {
-    ThreadPool pool(5);
+    ThreadPool pool(100);
 
-    int tasks_num = 5;
+    int tasks_num = 1000;
     for (int i = 0; i < tasks_num; i++) {
         pool.create_task();
     }
